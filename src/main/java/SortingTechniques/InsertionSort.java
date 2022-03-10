784c800ae0915a2c240955b49b45975a93e45006
Sunandan-Nakka <sunandan.nakka@thermofisher.com>
2022-03-10 23:19:23 +0530

    Insertion Sort Implementation.

---
+++ src/main/java/SortingTechniques/InsertionSort.java
@@ -0,0 +1,27 @@
+package SortingTechniques;
+
+public class InsertionSort {
+    private InsertionSort() { }
+
+    public static void Sort(Comparable[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (less(array[j], array[j - 1])) {
+                    exchange(array, j, j - 1);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    private static boolean less(Comparable a, Comparable b) {
+        return a.compareTo(b) < 0;
+    }
+
+    private static void exchange(Comparable[] array, int i, int j) {
+        Comparable temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
